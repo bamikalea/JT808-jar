382624572944c0bdb0532c7d76d0d5be56e20a00
bamikalea <amupitanbamikale@gmail.com>
2025-06-27 21:08:00 +0100

    Enable CORS for React dashboard (localhost:3000) in WebMvcConfig

--- jtt808-server/src/main/java/org/yzh/web/config/WebMvcConfig.java
+++ jtt808-server/src/main/java/org/yzh/web/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package org.yzh.web.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,5 +19,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowCredentials(true);
     }
 }
